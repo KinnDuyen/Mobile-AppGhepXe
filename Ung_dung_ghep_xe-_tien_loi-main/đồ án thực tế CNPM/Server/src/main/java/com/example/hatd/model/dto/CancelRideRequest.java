2159ad60a9a75e7c1d85777c1d99ba51d27572f2
KinnDuyen <ttkduyen040305@gmail.com>
2026-04-09 08:09:45 +0700

    Day code len repo moi UngDungGhepXe

--- Ung_dung_ghep_xe-_tien_loi-main/đồ án thực tế CNPM/Server/src/main/java/com/example/hatd/model/dto/CancelRideRequest.java
+++
@@ -1,9 +0,0 @@
-package com.example.hatd.model.dto;
-
-import lombok.Data;
-
-@Data
-public class CancelRideRequest {
-    private Long matchId; // Giống với trường trong Kotlin Client
-    private String reason; // Lý do hủy chuyến
-}
--- Ung_dung_ghep_xe-_tien_loi-main/đồ án thực tế CNPM/Server/src/main/java/com/example/hatd/repository/ViTriRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.hatd.repository;
-
-import com.example.hatd.model.ViTri;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ViTriRepository extends JpaRepository<ViTri, Long> {
-}
